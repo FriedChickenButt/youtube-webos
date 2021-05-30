c6f4180516d9d43d092ca6db4355b33ef529346a
Piotr Dobrowolski <admin@tastycode.pl>
2021-05-30 23:38:55 +0200

    userscript: add newer ad filtering based on uBlock Origin rules

--- webOSUserScripts/userScript.js
+++ webOSUserScripts/userScript.js
@@ -55,5 +55,22 @@ fetch = function () {
     return;
   }
   return origFetch.apply(this, arguments);
+};
 
+/**
+ * This is a minimal reimplementation of the following uBlock Origin rule:
+ * https://github.com/uBlockOrigin/uAssets/blob/3497eebd440f4871830b9b45af0afc406c6eb593/filters/filters.txt#L116
+ *
+ * This in turn calls the following snippet:
+ * https://github.com/gorhill/uBlock/blob/bfdc81e9e400f7b78b2abc97576c3d7bf3a11a0b/assets/resources/scriptlets.js#L365-L470
+ *
+ * Seems like for now dropping just the adPlacements is enough for YouTube TV
+ */
+const origParse = JSON.parse;
+JSON.parse = function () {
+  const r = origParse.apply(this, arguments);
+  if (r.adPlacements) {
+    r.adPlacements = [];
+  }
+  return r;
 };
