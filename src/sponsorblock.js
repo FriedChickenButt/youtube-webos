4910cffb055a5ec48cf1e688ede56c5aa87c4062
Piotr Dobrowolski <admin@tastycode.pl>
2021-06-09 08:11:41 +0200

    sponsorblock: fix back-to-back skips by increasing lookback window...

--- src/sponsorblock.js
+++ src/sponsorblock.js
@@ -165,7 +165,7 @@ class SponsorBlockHandler {
     // Sometimes timeupdate event (that calls scheduleSkip) gets fired right before
     // already scheduled skip routine below. Let's just look back a little bit
     // and, in worst case, perform a skip at negative interval (immediately)...
-    const nextSegments = this.segments.filter(seg => seg.segment[0] > this.video.currentTime - 0.1 && seg.segment[1] > this.video.currentTime - 0.1);
+    const nextSegments = this.segments.filter(seg => seg.segment[0] > this.video.currentTime - 0.3 && seg.segment[1] > this.video.currentTime - 0.3);
     nextSegments.sort((s1, s2) => s1.segment[0] - s2.segment[0]);
 
     if (!nextSegments.length) {
@@ -184,6 +184,7 @@ class SponsorBlockHandler {
       }
 
       const skipName = barTypes[segment.category]?.name || segment.category;
+      console.info(this.videoID, 'Skipping', segment);
       showNotification(`Skipping ${skipName}`);
       this.video.currentTime = end;
       this.scheduleSkip();
