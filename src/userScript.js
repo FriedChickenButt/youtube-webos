98f4459adb743f9b2086459a6a46a31f05c579ed
fire332 <96039230+fire332@users.noreply.github.com>
2022-01-08 07:19:17 -0800

    fix screensaver bug

--- src/userScript.js
+++ src/userScript.js
@@ -1,7 +1,7 @@
 import 'whatwg-fetch';
 import './domrect-polyfill';
 
-import { handleLaunch } from './utils';
+import { handleLaunch, waitForChildAdd } from './utils';
 
 document.addEventListener(
   'webOSRelaunch',
@@ -15,3 +15,37 @@ document.addEventListener(
 import './adblock.js';
 import './sponsorblock.js';
 import './ui.js';
+
+// This IIFE is to keep the video element fill the entire window so that screensaver doesn't kick in.
+(async () => {
+  /** @type {HTMLVideoElement} */
+  const video = await waitForChildAdd(
+    document.body,
+    (node) => node instanceof HTMLVideoElement
+  );
+
+  const playerCtrlObs = new MutationObserver(() => {
+    const style = video.style;
+
+    const targetWidth = `${window.innerWidth}px`;
+    const targetHeight = `${window.innerHeight}px`;
+    const targetLeft = '0px';
+    // YT uses a negative top to hide player when not in use. Don't know why but let's not affect it.
+    const targetTop =
+      style.top === `-${window.innerHeight}px` ? style.top : '0px';
+
+    /**
+     * Check to see if identical before assignment as some webOS versions will trigger a mutation
+     * mutation event even if the assignment effectively does nothing, leading to an infinite loop.
+     */
+    style.width !== targetWidth && (style.width = targetWidth);
+    style.height !== targetHeight && (style.height = targetHeight);
+    style.left !== targetLeft && (style.left = targetLeft);
+    style.top !== targetTop && (style.top = targetTop);
+  });
+
+  playerCtrlObs.observe(video, {
+    attributes: true,
+    attributeFilter: ['style']
+  });
+})();
