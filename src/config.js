968d8969ec379d0e6720e014199bdcb3f0c8bf5b
throwaway96 <68320646+throwaway96@users.noreply.github.com>
2024-03-24 20:57:20 -0400

    rework config system/UI
    Centralize all information about configuration options in a single
    object. Replace hardcoded options UI with programmatic generation based
    on descriptions in config.js. Also throw exceptions on invalid config
    keys and stop changes to localConfig from affecting defaultConfig.
    
    Ideally the options UI would be entirely automatically generated, but a
    way a way to handle grouped options (like the SponsorBlock ones) is
    still needed.
    
    It is now easier to add a config option, and hopefully soon it will only
    require adding an entry to configOptions in config.js.

--- src/config.js
+++ src/config.js
@@ -1,25 +1,81 @@
 const CONFIG_KEY = 'ytaf-configuration';
-const defaultConfig = {
-  enableAdBlock: true,
-  enableSponsorBlock: true,
-  enableSponsorBlockSponsor: true,
-  enableSponsorBlockIntro: true,
-  enableSponsorBlockOutro: true,
-  enableSponsorBlockInteraction: true,
-  enableSponsorBlockSelfPromo: true,
-  enableSponsorBlockMusicOfftopic: true
-};
 
-let localConfig;
+export const configOptions = new Map([
+  ['enableAdBlock', { default: true, desc: 'Enable ad blocking' }],
+  ['enableSponsorBlock', { default: true, desc: 'Enable SponsorBlock' }],
+  [
+    'enableSponsorBlockSponsor',
+    { default: true, desc: 'Skip sponsor segments' }
+  ],
+  ['enableSponsorBlockIntro', { default: true, desc: 'Skip intro segments' }],
+  ['enableSponsorBlockOutro', { default: true, desc: 'Skip outro segments' }],
+  [
+    'enableSponsorBlockInteraction',
+    {
+      default: true,
+      desc: 'Skip interaction reminder segments'
+    }
+  ],
+  [
+    'enableSponsorBlockSelfPromo',
+    {
+      default: true,
+      desc: 'Skip self promotion segments'
+    }
+  ],
+  [
+    'enableSponsorBlockMusicOfftopic',
+    {
+      default: true,
+      desc: 'Skip music and off-topic segments'
+    }
+  ]
+]);
 
-try {
-  localConfig = JSON.parse(window.localStorage[CONFIG_KEY]);
-} catch (err) {
-  console.warn('Config read failed:', err);
-  localConfig = defaultConfig;
+const defaultConfig = (() => {
+  let ret = {};
+  for (const [k, v] of configOptions) {
+    ret[k] = v.default;
+  }
+  return ret;
+})();
+
+function loadStoredConfig() {
+  const storage = window.localStorage.getItem(CONFIG_KEY);
+
+  if (storage === null) {
+    console.info('Config not set; using defaults.');
+    return null;
+  }
+
+  try {
+    return JSON.parse(storage);
+  } catch (err) {
+    console.warn('Error parsing stored config:', err);
+    return null;
+  }
+}
+
+// Use defaultConfig as a prototype so writes to localConfig don't change it.
+let localConfig = loadStoredConfig() ?? Object.create(defaultConfig);
+
+function configExists(key) {
+  return configOptions.has(key);
+}
+
+export function getConfigDesc(key) {
+  if (!configExists(key)) {
+    throw new Error('tried to get desc for unknown config key: ' + key);
+  }
+
+  return configOptions.get(key).desc;
 }
 
 export function configRead(key) {
+  if (!configExists(key)) {
+    throw new Error('tried to read unknown config key: ' + key);
+  }
+
   if (localConfig[key] === undefined) {
     console.warn(
       'Populating key',
@@ -27,6 +83,7 @@ export function configRead(key) {
       'with default value',
       defaultConfig[key]
     );
+
     localConfig[key] = defaultConfig[key];
   }
 
@@ -34,6 +91,10 @@ export function configRead(key) {
 }
 
 export function configWrite(key, value) {
+  if (!configExists(key)) {
+    throw new Error('tried to write unknown config key: ' + key);
+  }
+
   console.info('Setting key', key, 'to', value);
   localConfig[key] = value;
   window.localStorage[CONFIG_KEY] = JSON.stringify(localConfig);
