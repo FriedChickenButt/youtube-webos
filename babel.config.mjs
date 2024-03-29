8b6cc3f67b21f14e08a01835cc4e4a8d4dcec8e6
fire332 <96039230+fire332@users.noreply.github.com>
2024-03-29 03:48:13 -0700

    babel config: update for new options and cache based on plugin versions

--- babel.config.mjs
+++ babel.config.mjs
@@ -2,27 +2,39 @@ import Module from 'node:module';
 
 const require = Module.createRequire(import.meta.url);
 
-export default (api) => {
-  api.cache.never();
+// From: https://github.com/babel/babel-polyfills/blob/a5db9c31c5b5474b4018e6178bc40882fc3eb5bf/packages/babel-plugin-polyfill-corejs3/README.md#version
+
+const {
+  version: babelruntimeVersion
+} = require('@babel/runtime-corejs3/package.json');
+const { version: corejspureVersion } = require('core-js-pure/package.json');
+
+/** @type {import('@babel/core').ConfigFunction} */
+function makeConfig(api) {
+  api.cache.invalidate(() => babelruntimeVersion + corejspureVersion);
 
   return {
     // Fixes "TypeError: __webpack_require__(...) is not a function"
     // https://github.com/webpack/webpack/issues/9379#issuecomment-509628205
-    // https://babeljs.io/docs/en/options#sourcetype
+    // https://babel.dev/docs/options#sourcetype
     sourceType: 'unambiguous',
+    // https://babel.dev/docs/assumptions
+    assumptions: {
+      noNewArrows: true
+    },
     plugins: [
       [
         '@babel/plugin-transform-runtime',
         {
           regenerator: false,
-          version:
-            require('./package.json').dependencies['@babel/runtime-corejs3']
+          version: babelruntimeVersion
         }
       ],
       [
         'polyfill-corejs3',
         {
-          method: 'usage-pure'
+          method: 'usage-pure',
+          version: corejspureVersion
         }
       ],
       [
@@ -41,4 +53,6 @@ export default (api) => {
       ]
     ]
   };
-};
+}
+
+export default makeConfig;
