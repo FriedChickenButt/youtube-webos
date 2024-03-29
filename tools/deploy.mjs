a5e12dbd8ec799e856ad3202441d8e148bd7370a
fire332 <96039230+fire332@users.noreply.github.com>
2024-03-29 03:15:17 -0700

    update deploy script to point to new @webos-tools/cli

--- tools/deploy.mjs
+++ tools/deploy.mjs
@@ -4,7 +4,7 @@ process.exit(
   spawnSync(
     'node',
     [
-      './node_modules/@webosose/ares-cli/bin/ares-install.js',
+      './node_modules/@webos-tools/cli/bin/ares-install.js',
       `./youtube.leanback.v4_${process.env.npm_package_version}_all.ipk`
     ],
     { stdio: 'inherit' }
