0dcda87cba6482f662b0b59e0ced0dc3a26a9303
fire332 <96039230+fire332@users.noreply.github.com>
2024-03-29 08:56:20 -0700

    lint everything with eslint

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -2,38 +2,8 @@
 
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  root: true,
-
   env: {
     browser: true,
-    es2024: true
-  },
-
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    ecmaFeatures: {
-      impliedStrict: true
-    }
-  },
-
-  extends: ['eslint:recommended', 'prettier'],
-
-  rules: {
-    'no-var': 'error',
-    'no-await-in-loop': 'error',
-    'no-implicit-globals': ['error'],
-    'no-unused-vars': ['error', { vars: 'local', argsIgnorePattern: '^_' }],
-    'no-useless-rename': ['error'],
-    'arrow-body-style': ['error', 'as-needed'],
-    'no-lonely-if': 'error',
-    'prefer-object-has-own': 'error',
-    'prefer-exponentiation-operator': 'error',
-    'prefer-regex-literals': ['error', { disallowRedundantWrapping: true }],
-    'array-callback-return': ['error', { checkForEach: true, allowVoid: true }],
-    'no-constant-binary-expression': 'error', // default in 'eslint:recommended' since v9
-    'no-constructor-return': 'error',
-    'no-empty-static-block': 'error', // default in 'eslint:recommended' since v9
-    'no-unmodified-loop-condition': 'error'
+    node: false
   }
 };
