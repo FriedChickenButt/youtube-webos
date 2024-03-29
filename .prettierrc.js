c7ea790c94a99d8f8ede63196f6cf0e76b554d81
fire332 <96039230+fire332@users.noreply.github.com>
2024-03-29 03:14:13 -0700

    add override to retain formatting before prettier 3.2.5

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,13 @@
+/** @type {import('prettier').Config} */
 module.exports = {
   trailingComma: 'none',
-  singleQuote: true
+  singleQuote: true,
+  overrides: [
+    {
+      files: ['tsconfig.json', 'jsconfig.json', 'tsconfig.*.json'],
+      options: {
+        parser: 'jsonc'
+      }
+    }
+  ]
 };
